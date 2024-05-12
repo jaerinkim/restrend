04f19b5f7f5c9c777042be1c6cb6f67e135db55f
jaerin-kim <jaerin.kim@outlook.com>
2024-05-13 00:22:36 +0900

    extract.py 함수화 완료

--- code/extract.py
+++ code/extract.py
@@ -4,61 +4,62 @@
 
 import pandas as pd
 
-generalPath = "./data/fulldata_07_24_04_P_일반음식점.csv"
-smallPath = "./data/fulldata_07_24_05_P_휴게음식점.csv"
+## generalPath에 일반음식점 smallPath에 휴게음식점 경로를 입력함.
+## 전처리된 DataFrame을 출력함.
+def init(generalPath = "../../data/fulldata_07_24_04_P_일반음식점.csv", smallPath = "../../data/fulldata_07_24_05_P_휴게음식점.csv"):
+    general = pd.read_csv(generalPath, encoding='CP949')
+    small = pd.read_csv(smallPath, encoding='CP949')
 
-general = pd.read_csv(generalPath, encoding='CP949')
-small = pd.read_csv(smallPath, encoding='CP949')
+    ## 날짜를 timedate type으로 변환
+    general.dropna(subset=['인허가일자'], inplace=True)
+    general.loc[general['폐업일자'].isna(),'폐업일자'] = '2099-12-31'
+    small.dropna(subset=['인허가일자'], inplace=True)
+    small.loc[small['폐업일자'].isna(),'폐업일자'] = '2099-12-31'
 
-## 날짜를 timedate type으로 변환
-general.dropna(subset=['인허가일자'], inplace=True)
-general.loc[general['폐업일자'].isna(),'폐업일자'] = '2099-12-31'
-small.dropna(subset=['인허가일자'], inplace=True)
-small.loc[small['폐업일자'].isna(),'폐업일자'] = '2099-12-31'
+    datelist = ['인허가일자','폐업일자']
+    dfs = [general,small]
+    d29 = [2]
+    d30 = [4,6,9,11]
+    d31 = [1,3,5,7,8,10,12]
 
-datelist = ['인허가일자','폐업일자']
-dfs = [general,small]
-d29 = [2]
-d30 = [4,6,9,11]
-d31 = [1,3,5,7,8,10,12]
+    for i in datelist:
+        for j in dfs:
+            ## YYYY-MM-DD를 YYYYMMDD로 수정함
+            j[i] = j[i].str.replace('-','')
+            small[i] = j[i].str.replace('-','')
+            ## None 날짜를 19000101로 수정함
+            j.loc[j[i].isna(),i] = '19000101'
+            small.loc[small[i].isna(),i] = '19000101'    
+            ##날짜 중 2월31일 등 존재하지 않는 날짜를 수정함
+            wrong29 = (j[i].str[4:6].astype(int).isin(d29)) & (j[i].str[6:].astype(int)>28)
+            j.loc[wrong29,i] = j.loc[wrong29,i].str[:6]+'28'
+            wrong30 = (j[i].str[4:6].astype(int).isin(d30)) & (j[i].str[6:].astype(int)>30)
+            j.loc[wrong30,i] = j.loc[wrong30,i].str[:6]+'30'
+            wrong31 = (j[i].str[4:6].astype(int).isin(d31)) & (j[i].str[6:].astype(int)>31)
+            j.loc[wrong31,i] = j.loc[wrong31,i].str[:6]+'31'
+            wrong13 = j[i].str[4:6].astype(int)>12
+            j.loc[wrong13,i] = j.loc[wrong13,i].str[:4]+'0615'
+            wrong0 = j[i].str[4:6].astype(int)==0
+            j.loc[wrong0,i] = j.loc[wrong0,i].str[:4] + '01' + j.loc[wrong0,i].str[6:]
+            wrong00 = j[i].str[6:].astype(int)==0
+            j.loc[wrong00,i] = j.loc[wrong00,i].str[:6] + '01'
+            ## datetime 형식으로 변환
+            j[i] = pd.to_datetime(j[i], format='%Y%m%d')
 
-for i in datelist:
-    for j in dfs:
-        ## YYYY-MM-DD를 YYYYMMDD로 수정함
-        j[i] = j[i].str.replace('-','')
-        small[i] = j[i].str.replace('-','')
-        ## None 날짜를 19000101로 수정함
-        j.loc[j[i].isna(),i] = '19000101'
-        small.loc[small[i].isna(),i] = '19000101'    
-        ##날짜 중 2월31일 등 존재하지 않는 날짜를 수정함
-        wrong29 = (j[i].str[4:6].astype(int).isin(d29)) & (j[i].str[6:].astype(int)>28)
-        j.loc[wrong29,i] = j.loc[wrong29,i].str[:6]+'28'
-        wrong30 = (j[i].str[4:6].astype(int).isin(d30)) & (j[i].str[6:].astype(int)>30)
-        j.loc[wrong30,i] = j.loc[wrong30,i].str[:6]+'30'
-        wrong31 = (j[i].str[4:6].astype(int).isin(d31)) & (j[i].str[6:].astype(int)>31)
-        j.loc[wrong31,i] = j.loc[wrong31,i].str[:6]+'31'
-        wrong13 = j[i].str[4:6].astype(int)>12
-        j.loc[wrong13,i] = j.loc[wrong13,i].str[:4]+'0615'
-        wrong0 = j[i].str[4:6].astype(int)==0
-        j.loc[wrong0,i] = j.loc[wrong0,i].str[:4] + '01' + j.loc[wrong0,i].str[6:]
-        wrong00 = j[i].str[6:].astype(int)==0
-        j.loc[wrong00,i] = j.loc[wrong00,i].str[:6] + '01'
-        ## datetime 형식으로 변환
-        j[i] = pd.to_datetime(j[i], format='%Y%m%d')
+    df = general._append(small)
+    df.dropna(subset='사업장명', inplace=True)
+    df['광역'] = df['소재지전체주소'].str.split(' ').str[0]
+    df['시군구'] = df['소재지전체주소'].str.split(' ').str[1]
+    df['읍면동'] = df['소재지전체주소'].str.split(' ').str[2]
 
-df = general._append(small)
-df.dropna(subset='사업장명', inplace=True)
-df['광역'] = df['소재지전체주소'].str.split(' ').str[0]
-df['시군구'] = df['소재지전체주소'].str.split(' ').str[1]
-df['읍면동'] = df['소재지전체주소'].str.split(' ').str[2]
+    df.loc[df['광역']=="세종특별자치시",'읍면동'] = df.loc[df['광역']=="세종특별자치시",'시군구']
+    df.loc[df['광역']=="세종특별자치시",'시군구'] = "세종특별자치시"
 
-df.loc[df['광역']=="세종특별자치시",'읍면동'] = df.loc[df['광역']=="세종특별자치시",'시군구']
-df.loc[df['광역']=="세종특별자치시",'시군구'] = "세종특별자치시"
+    ## 일관성을 위해 일관되지 않은 주소들은 검색에서 제외했습니다. (예: 한국마사회)
+    df = df[df['광역'].isin(df['광역'].unique()[:16])]
+    df['지역'] = df['광역'] + ' ' + df['시군구'] + ' ' + df['읍면동']
+    return(df)
 
-## 일관성을 위해 일관되지 않은 주소들은 검색에서 제외했습니다. (예: 한국마사회)
-df = df[df['광역'].isin(df['광역'].unique()[:16])]
-
-df['지역'] = df['광역'] + ' ' + df['시군구'] + ' ' + df['읍면동']
 
 ## date부터 enddate까지 모든 날짜를 리스트로 출력. date와 endddate는 timestamp여야 함.
 def dates(date,enddate):
--- code/main.py
+++ code/main.py
@@ -1,1 +1,7 @@
-import extract
+import extract as ex
+
+df = ex.init()
+
+## 마라 예시
+# mala = ex.shopDates(df,['마라'],['중국식','일반조리판매'])
+# mala.to_csv("마라예시.csv",encoding='EUC-KR')
